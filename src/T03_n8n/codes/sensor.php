a2febe377befe3c94c0da2d0fc4b330ab8f8a448
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-09-21 06:59:26 +0700

    Add sensor.php to n8n codes

---
+++ src/T03_n8n/codes/sensor.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sensor</title>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.lime.css">
+    <script>
+        // This function will refresh the image every second
+        function refreshImage() {
+            const img = document.getElementById('sensor-img');
+            const baseUrl = 'https://pm1-ct102-n8n.iecmu.com/webhook/light';
+            // Add a timestamp to bypass browser cache
+            img.src = baseUrl + '?t=' + new Date().getTime();
+        }
+
+        // Turn LED ON
+        function turnLedOn() {
+            fetch('https://pm1-ct102-n8n.iecmu.com/webhook/led?led=on', {
+                    method: 'GET'
+                })
+                .then(response => {
+                    alert('LED turned ON');
+                });
+        }
+
+        // Turn LED OFF
+        function turnLedOff() {
+            fetch('https://pm1-ct102-n8n.iecmu.com/webhook/led?led=off', {
+                    method: 'GET'
+                })
+                .then(response => {
+                    alert('LED turned OFF');
+                });
+        }
+
+        // Start refreshing after the page loads
+        window.onload = function() {
+            refreshImage(); // Initial load
+            setInterval(refreshImage, 2000); // Repeat
+
+            document.getElementById('btn-on').addEventListener('click', turnLedOn);
+            document.getElementById('btn-off').addEventListener('click', turnLedOff);
+
+        };
+    </script>
+    </script>
+
+
+</head>
+
+<body>
+    <main class="container">
+        <h1>Sensor Value</h1>
+
+        <article>
+            <h2>Light Sensor</h2>
+            <img id="sensor-img" alt="Sensor Value">
+        </article>
+
+        <article>
+            <h2>Control</h2>
+            <div class="grid">
+                <button id="btn-on">ON</button>
+                <button id="btn-off">OFF</button>
+            </div>
+        </article>
+        </div>
+
+
+    </main>
+</body>
+
+</html>
